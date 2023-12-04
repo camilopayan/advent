8eaad8988fb0f465e0e22c4fe39eb9fc96b4f1c5
Camilo Payan <camilo.payan@testdouble.com>
2023-12-04 09:46:27 -0500

    refactored day 3 a little, still not working

--- 2023/03.rb
+++ 2023/03.rb
@@ -1,13 +1,17 @@
 sum = 0
 
+def get_all_matches(line, regex)
+  line.to_enum(:scan, regex).map { Regexp.last_match }
+end
+
 def line_sum(line)
   numbers = []
-  line.to_enum(:scan, /[^.\d\s](\d+)/).map { Regexp.last_match }.each do |match|
+  get_all_matches(line, /[^.\d\s](\d+)/).each do |match|
     num = match[1]
     numbers << num.to_s.to_i
     # line.sub!(/#{num}/, '.' * num.to_s.length)
   end
-  line.to_enum(:scan, /(\d+)[^.\d\s]/).map { Regexp.last_match }.each do |match|
+  get_all_matches(line, /(\d+)[^.\d\s]/).each do |match|
     num = match[1]
     numbers << num.to_s.to_i
     # line.sub!(/#{num}/, '.' * num.to_s.length)
@@ -19,27 +23,26 @@ def line_sum(line)
 end
 
 def symbol_positions(line)
-  matches = line.to_enum(:scan, /([^.\d\s])/).map { Regexp.last_match }
-  matches.map { |m| m.begin(1) }
+  get_all_matches(line, /([^.\d\s])/).map { |m| m.begin(1) }
 end
 
 def adjacents_sum(line, positions)
-  matches = line.to_enum(:scan, /(\d+)/).map { Regexp.last_match }
+  matches = get_all_matches(line, /(\d+)/)
   numbers = matches.filter do |match|
     positions.reduce(false) do |memo, pos|
       a, o = match.offset(0)
-      pos.between?(a-1, o+1) || memo
+      pos.between?(a - 1, o + 1) || memo
     end
   end
-  numbers.each do |number|
-    # line.sub!(/#{number[0]}/, '.' * number[0].to_s.length)
-  end
+  # numbers.each do |number|
+  #   line.sub!(/#{number[0]}/, '.' * number[0].to_s.length)
+  # end
   pp numbers
   sum = numbers.map { |m| m[0].to_i }.reduce(0, :+)
   p "Add on #{sum}"
   sum
 end
-#
+
 def process_three_lines(previous_line, current_line, next_line)
   sum = 0
   # Sum up all part numbers on current line
